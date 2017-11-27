89441203afa449a2ed71be729e4ba9c07dae5f0b
ignindra <ignindrakw@gmail.com>
2017-11-27 14:17:47 +0700

    fix indentation

--- Counter.js
+++ Counter.js
@@ -2,23 +2,23 @@
 import React, { Component, PropTypes } from 'react'
 
 const Counter = ({ value, onIncrement, onDecrement, onIncrementAsync }) =>
-      <div>
-        <button onClick={onIncrement}>
-          Increment
-        </button>
-        {' '}
-        <button onClick={onDecrement}>
-          Decrement
-        </button>
-        {' '}
-        <button onClick={onIncrementAsync}>
-          Increment after 1 second
-        </button>
-        <hr />
-        <div>
-          Clicked: {value} times
-        </div>
-      </div>
+  <div>
+    <button onClick={onIncrement}>
+      Increment
+    </button>
+    {' '}
+    <button onClick={onDecrement}>
+      Decrement
+    </button>
+    {' '} 
+    <button onClick={onIncrementAsync}>
+      Increment after 1 second
+    </button>
+    <hr />
+    <div>
+      Clicked: {value} times
+    </div>
+  </div>
 
 Counter.propTypes = {
   value: PropTypes.number.isRequired,
